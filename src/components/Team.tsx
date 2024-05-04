e54f2244b5bd9d337e257ce89a15ac562fb286d7
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2024-05-04 20:32:51 +0530

    some changes

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -7,11 +7,12 @@ import instagram from "../../public/instagram.svg";
 import linkedin from "../../public/linkedin.svg";
 import MemberPhotoWall from "./PhotoWall";
 
-
 export default function TeamMember(): ReactElement {
     const [selectedYear, setSelectedYear] = useState<string>(
         Object.keys(data)[0]
     );
+    const [selectedCardIndex, setSelectedCardIndex] = useState<number>(-1);
+
     return (
         <div id="team-member" className="max-w-[90vw] md:max-w-[1200px] mx-auto mt-28 mb-12">
             <MemberPhotoWall/> 
@@ -26,40 +27,46 @@ export default function TeamMember(): ReactElement {
                     </div>
                 ))}
             </div>
-            <h2 className="text-3xl text-gray-700 font-bold my-12">Member Introduction</h2>
+            <h2 className="text-3xl text-gray-700 font-bold my-12 text-center md:text-left">Member Introduction</h2>
             <div className="flex items-center justify-between flex-wrap gap-[3vw]">
                 {data[selectedYear].members.map((member, key) => (
-                    <div key={key} className={`flex w-[35rem] h-[min(21.5rem,50vw)] items-center justify-between rounded-[20px] border-b-4 shadow-md ${key % 3 === 0 ? 'border-b-5 border-google-red' : key % 3 === 1 ? 'border-b-5 border-google-green' : 'border-b-5 border-google-yellow'} hover:border-[5px] hover:border-[#ea4336|#34a852|#faab00] transition duration-300`}>
+                    <div 
+                        key={key} 
+                        className={`flex w-[35rem] h-[min(21.5rem,50vw)] items-center justify-between rounded-[20px] border-b-4 shadow-md ${key % 3 === 0 ? 'border-b-5 border-google-red' : key % 3 === 1 ? 'border-b-5 border-google-green' : 'border-b-5 border-google-yellow'} hover:border-[5px] hover:border-[#ea4336|#34a852|#faab00] transition duration-300 `}
+                        onMouseEnter={() => setSelectedCardIndex(key)}
+                        onMouseLeave={() => setSelectedCardIndex(-1)}
+                    >
                         <img
                             className="w-[32.5%] h-[60%]"
                             src={member.photo}
                             alt="ERROR"
                         />
-                        <div className="flex flex-col w-[50%] h-[70%] justify-between">
-                            <p className="h-[10%] mb-0 font-medium text-[1rem]">{member.name}</p>
-                            <p className="h-[10%] mb-0 font-medium text-[1rem]">{member.jobTitle}</p>
-                            <p className="h-[10%] mb-0 font-medium text-[1rem]">{member.department}</p>
-                            <p className="font-normal">{member.introduction}</p>
+                        <div className="flex flex-col w-[60%] h-[70%] justify-around p-2 ">
+                            <p className="h-[10%] mb-0 font-bold md:text-xl text-md">{member.name}</p>
+                            <p className={`h-[10%] mb-4 md:mb-0 py-2 md:py-0 font-semibold text-[0.8rem] md:text-[1.2rem]  text-google-blue ${key % 3 === 0 ? ' text-google-red' : key % 3 === 1 ? 'text-google-green' : 'text-google-yellow'}`}>
+                                {member.jobTitle}
+                            </p>
+                            <p className={`h-[20%] hidden md:block overflow-${selectedCardIndex === key ? 'y-scroll' : 'hidden'} text-[#5f6367] font-normal`}>{member.introduction}</p>
                             <div className="flex h-[15%] gap-[1.5rem]">
                                 <a href={member.instagram} target="_blank">
                                     <Image
                                         src={instagram}
                                         alt="ERROR"
-                                        className="w-[1.75rem] h-[1.75rem]"
+                                        className="w-[1.3rem] h-[1.3rem] md:w-[1.75rem] md:h-[1.75rem]"
                                     />
                                 </a>
                                 <a href={member.github} target="_blank">
                                     <Image
                                         src={github}
                                         alt="ERROR"
-                                        className="w-[1.75rem] h-[1.75rem]"
+                                        className="w-[1.3rem] h-[1.3rem] md:w-[1.75rem] md:h-[1.75rem]"
                                     />
                                 </a>
                                 <a href={member.linkedin} target="_blank">
                                     <Image
                                         src={linkedin}
                                         alt="ERROR"
-                                        className="w-[1.75rem] h-[1.75rem]"
+                                        className="w-[1.3rem] h-[1.3rem] md:w-[1.75rem] md:h-[1.75rem]"
                                     />
                                 </a>
                             </div>
